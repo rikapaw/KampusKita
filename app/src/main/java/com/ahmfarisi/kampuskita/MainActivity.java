dff6f6d49058e6fb83ddf3fa51511710fd4f2e47
ahmfarisi <ahmfarisi@gmail.com>
2022-11-10 12:39:35 +0700

    create method for opening fragment

--- app/src/main/java/com/ahmfarisi/kampuskita/MainActivity.java
+++ app/src/main/java/com/ahmfarisi/kampuskita/MainActivity.java
@@ -1,16 +1,31 @@
 package com.ahmfarisi.kampuskita;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Menu;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bnvKampusKita;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bnvKampusKita = findViewById(R.id.bnv_kampus_kita);
+    }
+
+    private void bukaFragment(Fragment varFragment){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fl_container, varFragment);
+        ft.commit();
     }
 
     @Override
